ca805741f2ada4479e6ac1dbd96ca086db1724fa
Joeny Bui <jbui@protection-consultants.com>
2016-02-02 09:51:37 -0600

    Skeleton structure.

--- peui/__init__.py
+++ peui/__init__.py
@@ -1,1 +1,8 @@
-__author__ = 'jbui'
+__title__ = 'Protection Engineering User Interface'
+__version__ = '0.0.1'
+__author__ = 'Joeny Bui'
+__license__ = 'proprietary'
+__copyright__ = 'Copyright @ Protection Engineering Consultants, LLC'
+
+# Version synonym
+VERSION = __version__
