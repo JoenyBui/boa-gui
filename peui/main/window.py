323ebc8ecf5dae5f142b593b7fb555a0e10c42fd
Joeny Bui <jbui@protection-consultants.com>
2016-02-03 22:44:27 -0600

    Update the library.

--- peui/main/window.py
+++ peui/main/window.py
@@ -25,82 +25,54 @@ class MainWindow(wx.Frame):
 
         # notify AUI which frame to use
         self._mgr.SetManagedWindow(self)
-
-        # create several text controls
-        text1 = wx.TextCtrl(self, -1, 'Pane 1 - sample text',
-                            wx.DefaultPosition, wx.Size(200,150),
-                            wx.NO_BORDER | wx.TE_MULTILINE)
-
-        text2 = wx.TextCtrl(self, -1, 'Pane 2 - sample text',
-                            wx.DefaultPosition, wx.Size(200,150),
-                            wx.NO_BORDER | wx.TE_MULTILINE)
-
-        text3 = wx.TextCtrl(self, -1, 'Main content window',
-                            wx.DefaultPosition, wx.Size(200,150),
-                            wx.NO_BORDER | wx.TE_MULTILINE)
-
-        self._tree = wx.TreeCtrl(self, -1)
-
-        # add the panes to the manager
-        self._mgr.AddPane(text1, wx.LEFT, 'Pane Number One')
-        self._mgr.AddPane(text2, wx.BOTTOM, 'Pane Number Two')
-        self._mgr.AddPane(text3, wx.CENTER)
-        self._mgr.AddPane(self._tree, wx.RIGHT, 'COME ON')
-
-        # Notebook
-        self._mgr.AddPane(GeneralPanel(self), wx.CENTER)
-        # self._notebook = wx.aui.AuiNotebook(self)
-        # self._panelOne = GeneralPanel(self._notebook)
-        # self._panelTwo = GeneralPanel(self._notebook)
-        #
-        # self._mgr.AddPane(self._notebook, wx.CENTER)
-                          # wx.aui.AuiPaneInfo().Name("notebook_content").CenterPane().PaneBorder(False))
-        # self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-
-        # self._mgr.AddPane(self)
-        # tell the manager to 'commit' all the changes just made
         self._mgr.Update()
 
-        self.Bind(wx.EVT_CLOSE, self.OnClose)
+        self.Bind(wx.EVT_CLOSE, self.on_close)
 
-        # self._notebook.Set
-
-        # # self.CreateStatusBar()
-        #
-        # filemenu = wx.Menu()
-        #
-        # menuAbout = filemenu.Append(wx.ID_ABOUT, "&About", " Information about this program.")
-        # filemenu.AppendSeparator()
-        # menuExit = filemenu.Append(wx.ID_EXIT, "E&xit", "Terminate the program")
-        #
-        # # Creating the menubar.
-        # menuBar = wx.MenuBar()
-        # menuBar.Append(filemenu, "&File")
         self.menu_bar = None
-
-
-        # self.SetMenuBar(self._menu_bar)
-
-        # # Set events.
-        # self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
-        # self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
 
         # Status Bar
         self.status_bar = CustomStatusBar(self)
         self.SetStatusBar(self.status_bar)
 
+    def add_pane(self, panel, area, name):
+        """
+        Add the pane panel.
+        :param panel:
+        :param area: wx.LEFT, wx.RIGHT, wx.BOTTOM, wx.TOP, wx.CENTER
+        :param name:
+        :return:
+        """
+        self._mgr.AddPane(panel, area, name)
+        self._mgr.Update()
 
-    def OnAbout(self, e):
+    def on_about(self, event):
+        """
+
+        :param event:
+        :return:
+        """
         dlg = wx.MessageDialog(self, "A small text editor", "About Sample Editor", wx.OK)
         dlg.ShowModal()
         dlg.Destroy()
 
-    def OnExit(self, e):
+    def on_exit(self, event):
+        """
+
+        :param event:
+        :return:
+        """
         self.Close(True)
 
-    def OnClose(self, event):
+    def on_close(self, event):
+        """
+
+        :param event:
+        :return:
+        """
         # De-initialize the frame manager
         self._mgr.UnInit()
+
         # delete the frame
         self.Destroy()
         event.Skip()
--- peui/tree/project.py
+++ peui/tree/project.py
@@ -8,7 +8,9 @@ class ProjectTree(wx.TreeCtrl):
     Project Tree Structure
     """
 
-    def __init__(self, parent, id, project):
-        wx.TreeCtrl.__init__(self, parent, id)
+    def __init__(self, parent, controller, project, **kwargs):
+        id = kwargs.get('id', -1)
+        wx.TreeCtrl.__init__(self, parent, id=id)
 
         self.project = project
+        self.controller = controller
