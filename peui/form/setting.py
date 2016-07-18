8e36a0e87795d14da1420bc25f6760b35c68962d
Unwana Nwoko <contractor_pec@protection-consultants.com>
2016-07-18 15:00:47 -0500

    Made the settings form for pec_gui

--- peui/form/setting.py
+++ peui/form/setting.py
@@ -1,5 +1,11 @@
 import wx
 
+from peui.textbox import LayoutDimensions
+from peui.textbox.floatbox import FloatInputLayout
+from peui.textbox.pathbox import PathInputLayout, PathSmartBox
+from peui.textbox.smart import SmartComboBox
+from peui.textbox.textbox import TextInputLayout
+
 from ..controller import ChildController
 
 __author__ = 'jbui'
@@ -17,6 +23,7 @@ class BaseSettingDialog(wx.Dialog):
         self.parent = parent
         self.controller = controller
 
+        self.nb = wx.Notebook(self)
         if local:
             self.local = local
         else:
@@ -31,10 +38,24 @@ class BaseSettingDialog(wx.Dialog):
         vsizer.AddStretchSpacer()
         vsizer.Add(self.btnsizer, 0, wx.EXPAND | wx.ALL, 5)
 
+        self.Bind(wx.EVT_BUTTON, self.on_okay, id=wx.ID_OK)
         self.SetSizer(vsizer)
         self.SetInitialSize()
         self.CenterOnParent()
         self.Fit()
+
+        self.local.load_component()
+
+    def on_okay(self, event):
+        """
+        On okay event.
+
+        :param event:
+        :return:
+        """
+        self.local.set_component()
+
+        event.Skip()
 
 
 class ControllerBaseDialog(ChildController):
@@ -58,13 +79,94 @@ class ControllerBaseDialog(ChildController):
         """
         ChildController.__init__(self, parent, view, *args, **kwargs)
 
+        self.parent = parent
+        self.view = view
+
     def sync_data(self):
         pass
 
-    def do_layout(self):
-        vsizer = wx.BoxSizer(wx.VERTICAL)
+    def settings_layout(self, parent_pnl):
+        """
+        Material Layout
+        """
+        # Panel
+        pnl = wx.ScrolledWindow(parent_pnl, id=wx.ID_ANY, style=wx.VSCROLL)
+        pnl.SetScrollRate(5, 5)
 
-        return vsizer
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        tb_layout = LayoutDimensions(top=2, bottom=2, right=4, left=4, widths=(150, 195, 75), interior=5,
+                                     stretch_factor=(1, 0, 0))
+        tb_layout.calculate()
+        cb_layout = LayoutDimensions(top=2, bottom=2, right=4, left=4, widths=(150, 275), interior=5,
+                                     stretch_factor=(1, 0))
+        cb_layout.calculate()
+
+        # Author
+        self.view.layouts['author'] = TextInputLayout(pnl, name='Author:', layout=cb_layout)
+
+        # Project Name
+        self.view.layouts['project_name'] = TextInputLayout(pnl, name='Project Name:', layout=cb_layout)
+
+        # Path
+        self.view.layouts['path'] = PathInputLayout(pnl, name='Path:', layout=tb_layout)
+
+        # E-Signature
+        self.view.layouts['esignature'] = PathInputLayout(pnl, name='E-Signature:', layout=tb_layout)
+
+        # E-Key
+        self.view.layouts['ekey'] = PathInputLayout(pnl, name='E-Key:', layout=tb_layout)
+
+        # E-File
+        self.view.layouts['efile'] = PathInputLayout(pnl, name='E-File:', layout=tb_layout)
+
+        sizer.AddSpacer(5)
+        sizer.Add(self.view.layouts['author'], 0, wx.ALL | wx.EXPAND, 0)
+        sizer.AddSpacer(5)
+        sizer.Add(self.view.layouts['project_name'], 0, wx.ALL | wx.EXPAND, 0)
+        sizer.AddSpacer(5)
+        sizer.Add(self.view.layouts['path'], 0, wx.ALL | wx.EXPAND, 0)
+        sizer.AddSpacer(5)
+        sizer.Add(self.view.layouts['esignature'], 0, wx.ALL | wx.EXPAND, 0)
+        sizer.AddSpacer(5)
+        sizer.Add(self.view.layouts['ekey'], 0, wx.ALL | wx.EXPAND, 0)
+        sizer.AddSpacer(5)
+        sizer.Add(self.view.layouts['efile'], 0, wx.ALL | wx.EXPAND, 0)
+        sizer.AddSpacer(5)
+        sizer.AddStretchSpacer(1)
+
+        pnl.SetSizer(sizer)
+
+        return pnl
+
+    def do_layout(self):
+        sizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.view.layouts['settings'] = self.settings_layout(self.view.nb)
+
+        self.view.nb.AddPage(self.view.layouts['settings'], 'Settings')
+
+        sizer.Add(self.view.nb, 1, wx.ALL | wx.EXPAND, 0)
+
+        sizer.SetMinSize(wx.Size(600, 400))
+
+        return sizer
+
+    def load_component(self):
+        self.view.layouts['author'].textbox.Value = str(self.view.controller.setting.author)
+        self.view.layouts['project_name'].textbox.Value = str(self.view.controller.setting.project_name)
+        self.view.layouts['path'].textbox.Value = str(self.view.controller.setting.path)
+        self.view.layouts['esignature'].textbox.Value = str(self.view.controller.setting.esignature)
+        self.view.layouts['ekey'].textbox.Value = str(self.view.controller.setting.ekey)
+        self.view.layouts['efile'].textbox.Value = str(self.view.controller.setting.efile)
+
+    def set_component(self):
+        self.view.controller.setting.author = self.view.layouts['author'].textbox.Value
+        self.view.controller.setting.project_name = self.view.layouts['project_name'].textbox.Value
+        self.view.controller.setting.path = self.view.layouts['path'].textbox.Value
+        self.view.controller.setting.esignature = self.view.layouts['esignature'].textbox.Value
+        self.view.controller.setting.ekey = self.view.layouts['ekey'].textbox.Value
+        self.view.controller.setting.efile = self.view.layouts['efile'].textbox.Value
 
     def update_layout(self):
         pass
