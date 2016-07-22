e2a9f50df1b0c16f41d60d8ab0810f107e366cf0
Joeny Bui <jbui@protection-consultants.com>
2016-07-22 16:38:42 -0500

    Add in new cast type.

--- peui/types.py
+++ peui/types.py
@@ -1,12 +1,21 @@
-from .units.length import LENGTH_KEY
+from .units.acceleration import ACCELERATION_KEY
 from .units.area import AREA_KEY
+from .units.area_density import AREA_DENSITY
 from .units.charge import CHARGE_KEY
-from .units.inertia import INERTIA_KEY
-from .units.mass import MASS_KEY
-from .units.volume import VOLUME_KEY
-from .units.pressure import PRESSURE_KEY
 from .units.density import DENSITY_KEY
+from .units.force import FORCE_KEY
+from .units.inertia import INERTIA_KEY
+from .units.length import LENGTH_KEY
+from .units.linear_density import LINEAR_DENSITY_KEY
+from .units.linear_pressure import LINEAR_PRESSURE_KEY
+from .units.mass import MASS_KEY
+from .units.pressure import PRESSURE_KEY
+from .units.specific_weight import SPECIFIC_WEIGHT_KEY
+from .units.time import TIME_KEY
+from .units.tnt import TNT_KEY
 from .units.torque import TORQUE_KEY
+from .units.velocity import VELOCITY_KEY
+from .units.volume import VOLUME_KEY
 
 
 def is_float(string):
@@ -67,19 +76,10 @@ def cast_float(string):
         return None
 
 
-def cast_length_unit(string):
-    """
-
-    :param string:
-    :return:
-    """
-    possible_units = LENGTH_KEY.keys()
-
-    if string in possible_units:
+def cast_acceleration_unit(string):
+    if string in ACCELERATION_KEY.keys():
         return string
     else:
-        # TODO: Write out to log file
-
         return None
 
 
@@ -99,19 +99,45 @@ def cast_area_unit(string):
         return None
 
 
+def cast_area_density_unit(string):
+    if string in AREA_DENSITY.keys():
+        return string
+    else:
+        return None
+
+
 def cast_charge_unit(string):
     """
 
     :param string:
     :return:
     """
-    possible_units = CHARGE_KEY.keys()
+    if string in CHARGE_KEY.keys():
+        return string
+    else:
+        return None
+
+
+def cast_density_unit(string):
+    """
+
+    :param string:
+    :return:
+    """
+    possible_units = DENSITY_KEY.keys()
 
     if string in possible_units:
         return string
     else:
         # TODO: Write out to log file
 
+        return None
+
+
+def cast_force_unit(string):
+    if string in FORCE_KEY.keys():
+        return string
+    else:
         return None
 
 
@@ -131,13 +157,13 @@ def cast_inertia_unit(string):
         return None
 
 
-def cast_mass_unit(string):
+def cast_length_unit(string):
     """
 
     :param string:
     :return:
     """
-    possible_units = MASS_KEY.keys()
+    possible_units = LENGTH_KEY.keys()
 
     if string in possible_units:
         return string
@@ -147,13 +173,27 @@ def cast_mass_unit(string):
         return None
 
 
-def cast_volume_unit(string):
+def cast_linear_density(string):
+    if string in LINEAR_DENSITY_KEY.keys():
+        return string
+    else:
+        return None
+
+
+def cast_linear_pressure(string):
+    if string in LINEAR_PRESSURE_KEY.keys():
+        return string
+    else:
+        return None
+
+
+def cast_mass_unit(string):
     """
 
     :param string:
     :return:
     """
-    possible_units = VOLUME_KEY.keys()
+    possible_units = MASS_KEY.keys()
 
     if string in possible_units:
         return string
@@ -179,19 +219,28 @@ def cast_pressure_unit(string):
         return None
 
 
-def cast_density_unit(string):
-    """
+def cast_specific_weight_unit(string):
+    if string in SPECIFIC_WEIGHT_KEY.keys():
+        return string
+    else:
+        return None
 
-    :param string:
-    :return:
-    """
-    possible_units = DENSITY_KEY.keys()
+
+def cast_time_unit(string):
+    possible_units = TIME_KEY.keys()
 
     if string in possible_units:
         return string
     else:
-        # TODO: Write out to log file
+        #TODO: Write out time unit
 
+        return None
+
+
+def cast_tnt_unit(string):
+    if string in TNT_KEY.keys():
+        return string
+    else:
         return None
 
 
@@ -202,6 +251,29 @@ def cast_torque_unit(string):
     :return:
     """
     possible_units = TORQUE_KEY.keys()
+
+    if string in possible_units:
+        return string
+    else:
+        # TODO: Write out to log file
+
+        return None
+
+
+def cast_velocity_unit(string):
+    if string in VELOCITY_KEY.keys():
+        return string
+    else:
+        return None
+
+
+def cast_volume_unit(string):
+    """
+
+    :param string:
+    :return:
+    """
+    possible_units = VOLUME_KEY.keys()
 
     if string in possible_units:
         return string
